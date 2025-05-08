b2e7d5579ad416d50cc6c2dd16eac10326a4f3e2
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-05-08 16:54:07 -0400

    Agrega funciones utilizadas en post-activity

--- flask_app/utils/utils.py
+++ flask_app/utils/utils.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+import os
+import hashlib
+import filetype
+from werkzeug.utils import secure_filename
+
 def formateaFechaHoraActividad(actividad):
     fechaInicio = actividad.dia_hora_inicio
     fechaTermino = actividad.dia_hora_termino
@@ -43,3 +49,63 @@ def recuperaContacto(request, data):
         contactos.append({'nombre': 'otra', 'identificador': otra_val})
     data['contactar_por'] = contactos
     return data
+
+def estructuraRequest(request):
+    data = {
+        "region_id" : request.form.get("region"), #retorna el id
+        "comuna_id" : request.form.get("comuna"), #retorna el id
+        "sector" : request.form.get("sector"), #retorna un string
+        "nombre" : request.form.get("nombre"), #retorna string
+        "email" : request.form.get("email"), #string email
+        "celular" : request.form.get("celular"), #string numero
+        "fecha_inicio" : request.form.get("fechaInicio"), #2025-05-03T00:45
+        "fecha_termino" : request.form.get("fechaTermino"),
+        "descripcion" : request.form.get("descripcion"), #string
+        "tema" : request.form.get("tema"), #string,
+        "descripcion_tema" : request.form.get("descripcion-tema"),
+        "fotos" : [request.files.get(f"foto{i}") for i in range(1,6)], #none
+        "rutas_fotos":[]
+    }
+    return recuperaContacto(request, data)
+
+def formatRequest(request):
+    data = {
+        "region_id" : int(request.form.get("region")), #retorna el id
+        "comuna_id" : int(request.form.get("comuna")), #retorna el id
+        "sector" : request.form.get("sector"), #retorna un string
+        "nombre" : request.form.get("nombre"), #retorna string
+        "email" : request.form.get("email"), #string email
+        "celular" : request.form.get("celular"), #string numero
+        "fecha_inicio" : datetime.fromisoformat(request.form.get("fechaInicio")), #2025-05-03T00:45
+        "fecha_termino" : None if request.form.get("fechaTermino") == '' else datetime.fromisoformat(request.form.get("fechaTermino")),
+        "descripcion" : request.form.get("descripcion"), #string
+        "tema" : request.form.get("tema"), #string,
+        "descripcion_tema" : request.form.get("descripcion-tema"),
+        "fotos" : [request.files.get(f"foto{i}") for i in range(1,6)], #none
+        "rutas_fotos":[]
+    }
+    return recuperaContacto(request, data)
+
+
+def guardaArchivos(data, app):
+    for i in range(5):
+        if data['fotos'][i].filename != '':
+            # 1. generate random name for img
+            _filename = hashlib.sha256(
+                secure_filename(data['fotos'][i].filename) # nombre del archivo
+                .encode("utf-8") # encodear a bytes
+                ).hexdigest()
+            _extension = filetype.guess(data['fotos'][i]).extension
+            img_filename = f"{_filename}.{_extension}"
+
+            # 2. save img as a file
+            ruta = os.path.join(app.config["UPLOAD_FOLDER"], img_filename)
+            try:
+                data['fotos'][i].save(ruta)
+                
+            except Exception as e:
+                msg_error = f"Error al guardar la imagen: {e}"
+                print(msg_error)
+                return {}, msg_error
+            data['rutas_fotos'].append({'ruta': ruta, 'nombre': img_filename})
+    return data, None
