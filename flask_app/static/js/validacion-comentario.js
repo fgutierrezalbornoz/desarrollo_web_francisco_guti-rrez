015769b867dedc476c4f009b89dadd8e8fd9593c
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-06-05 23:48:43 -0400

    Corrige script que postea la información de comentarios, ahora sí usa AJAX

--- flask_app/static/js/validacion-comentario.js
+++ flask_app/static/js/validacion-comentario.js
@@ -10,8 +10,7 @@ const validaComentario = (comentario) => {
     if(!comentario) return false;
     return comentario.trim().length >= 5;
 }
-const validateCommentForm = () => {
-    console.log("comienza a validar")
+const validateCommentForm = async () => {
     let formComentario = document.forms["form-comentario"];
     let nombre = formComentario["nombre"].value
     let comentario = formComentario["comentario"].value
@@ -51,9 +50,60 @@ const validateCommentForm = () => {
       // hacer visible el mensaje de validación
       validationBox.hidden = false;
     } else {
-        form.submit();
+        const formData = new FormData(form);
+        try {
+            const response = await fetch(form.action, {
+                method: "POST",
+                body: formData,
+            });
+            if (response.ok) {
+                form.reset();
+                cargarComentarios();
+            }
+        } catch (error) {
+            validationBox.hidden = false;
+            validationBox.style.backgroundColor = "#ffdddd";
+            validationBox.style.borderLeftColor = "#f44336";
+            validationMessageElem.innerText = "Error de red.";
+            validationListElem.textContent = "";
+            console.error("Error al conectar con el servidor: ", error)
+        }
     }
 }
 
 let submitBtn = document.getElementById("submit-comment-btn");
 submitBtn.addEventListener("click", validateCommentForm);
+
+async function cargarComentarios() {
+    const match = window.location.pathname.match(/\/activity\/(\d+)/);
+    if (!match) return;
+    const actividadId = match[1];
+
+    const container = document.querySelector('.comentario-container');
+    container.innerHTML = "Cargando comentarios...";
+
+    try {
+        const resp = await fetch(`/get-comments/${actividadId}`);
+        const comentarios = await resp.json();
+        if (comentarios.length === 0) {
+            container.innerHTML = "<p>No hay comentarios aún.</p>";
+            return;
+        }
+        container.innerHTML = "";
+        comentarios.forEach(c => {
+            const div = document.createElement('div');
+            div.className = "comentario";
+            fechaLocal = new Date(c.fecha).toLocaleString("es-CL", {
+                dateStyle: "short",
+                timeStyle: "short"
+            });
+            div.innerHTML = `${fechaLocal} </br> <strong>${c.nombre}</strong>: ${c.comentario} <hr>`;
+            container.appendChild(div);
+        });
+    } catch (e) {
+        container.innerHTML = "<p>Error al cargar comentarios.</p>";
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', cargarComentarios);
