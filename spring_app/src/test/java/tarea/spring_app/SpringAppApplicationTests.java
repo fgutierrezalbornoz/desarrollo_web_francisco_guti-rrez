5b068ea16ffae92d18d35dea748a2e2f2d322a02
fgutierrezalbornoz <francisco.gutial@gmail.com>
2025-06-26 21:15:18 -0400

    Crea Proyecto SpringBoot

---
+++ spring_app/src/test/java/tarea/spring_app/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea.spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
